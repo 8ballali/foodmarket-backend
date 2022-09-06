9df2abc121eb3ab39b08752e300849fa8792485d
8ballali <iqbalalisaidil@outlook.com>
2022-09-06 18:23:43 +0700

    Add Transaction API

---
+++ app/Http/Controllers/API/MidtransController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class MidtransController extends Controller
+{
+    public function callback(Request $requeste)
+    {
+        //Konfigurasi Midtrans
+    }
+}
